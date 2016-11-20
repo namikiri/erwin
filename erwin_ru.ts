21ca32f49fe4c2fc1eed98891a310832ce71c290
namikiri <aki@densetos.com>
2016-11-20 22:05:41 +0300

    Updated language files

--- erwin_ru.ts
+++ erwin_ru.ts
@@ -2,18 +2,18 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
-    <name>BotHunter</name>
+    <name>Erwin</name>
     <message>
         <source>* Banned %1 (UID %2) for prisoning. Please report the issue to SAA7134 if this was wrong.</source>
         <translation type="vanished">* %1 (UID %2) заблокирован за посадку. Если вы считаете, что это сделано зря, напишите в приват персонажу SAA7134.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="145"/>
+        <location filename="erwin.cpp" line="134"/>
         <source>Hallo meine lieben %1 :3</source>
         <translation>Привет, прекрасный %1 :3</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="287"/>
+        <location filename="erwin.cpp" line="277"/>
         <source>%5, banned %1 bots (%2 directly, %3 with deferred blacklisting). %7 users banned for prisoning, %4 bot calls detected, %6 users on &apos;%8&apos; (ChannelID %9) are currently online.</source>
         <oldsource>%5, banned %1 bots (%2 directly, %3 with deferred blacklisting). %7 users banned for prisoning, %4 bot calls detected, %6 users on &apos;%7&apos; (ChannelID %8) are currently online.</oldsource>
         <translation>%5, заблокировано %1 ботов (%2 напрямую, %3 отложенным способом). %7 пользователей заблокировано за посадку, %4 обращений к боту, %6 человек на &apos;%8&apos; (ChannelID %9) онлайн.</translation>
@@ -24,190 +24,189 @@
         <translation type="vanished">%5, настройки демона: autokick %1; autoban %2; prisonban %3; admin_id %4; companion_id %8; stats_delay %6s; planet &apos;%7&apos;</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="227"/>
+        <location filename="erwin.cpp" line="217"/>
         <source>* Banned %1 (UID %2) for prisoning. Please report the issue to %3 if this was wrong.</source>
         <translation>* %1 (UID %2) заблокирован за посадку. Если вы считаете, что это сделано зря, напишите в приват персонажу %3.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="110"/>
+        <location filename="erwin.cpp" line="99"/>
         <source>* %1 (UID %3) has been banned because he/she is a member of clan %2. If this is wrong, please send a message to %4</source>
         <translation>* %1 (UID %3) заблокирован за членство в клане %2 (скорее всего сажают). Если есть вопросы, пишите в приват персонажу %4</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="300"/>
+        <location filename="erwin.cpp" line="290"/>
         <source>%2, daemon configuration: %1</source>
         <translation>%2, настройки сервиса: %1</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="336"/>
+        <location filename="erwin.cpp" line="326"/>
         <source>%1, tried to kick ALL from the planet.</source>
         <translation>%1, пытаемся выкинуть всех с планеты.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="343"/>
-        <location filename="bothunter.cpp" line="373"/>
-        <location filename="bothunter.cpp" line="391"/>
-        <location filename="bothunter.cpp" line="419"/>
-        <location filename="bothunter.cpp" line="437"/>
+        <location filename="erwin.cpp" line="333"/>
+        <location filename="erwin.cpp" line="363"/>
+        <location filename="erwin.cpp" line="381"/>
+        <location filename="erwin.cpp" line="409"/>
+        <location filename="erwin.cpp" line="427"/>
         <source>%1, no such nick on the planet.</source>
         <translation>%1, такого персонажа на планете нет.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="347"/>
+        <location filename="erwin.cpp" line="337"/>
         <source>%1, successfully kicked %2 (UID %3)</source>
         <translation>%1, успешно выгнали %2 (UID %3)</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="366"/>
+        <location filename="erwin.cpp" line="356"/>
         <source>%1, tried to ban ALL from the planet.</source>
         <translation>%1, пытаемся забанить всех на планете.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="377"/>
+        <location filename="erwin.cpp" line="367"/>
         <source>%1, successfully banned %2 (UID %3)</source>
         <translation>%1, успешно забанили %2 (UID %3)</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="394"/>
+        <location filename="erwin.cpp" line="384"/>
         <source>%1, UID of %2 is %3</source>
         <translation>%1, идентификатор %2 - %3</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="406"/>
+        <location filename="erwin.cpp" line="396"/>
         <source>%1, joining the channel %2...</source>
         <translation>%1, перелетаем на планету %2...</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="422"/>
+        <location filename="erwin.cpp" line="412"/>
         <source>%1, granted admin rights to %2 (UID %3)</source>
         <translation>%1, права администратора переданы %2 (UID %3)</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="440"/>
+        <location filename="erwin.cpp" line="430"/>
         <source>%1, my companion is %2 (UID %3) now.</source>
         <translation>%1, теперь мой товарищ %2 (UID %3).</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="450"/>
+        <location filename="erwin.cpp" line="440"/>
         <source>%1, available commands are: %2</source>
         <translation>%1, доступные команды: %2</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="460"/>
+        <location filename="erwin.cpp" line="450"/>
         <source>%1, switching to the next recovery code...</source>
         <translation>%1, переключаюсь на следующий код восстановления...</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="464"/>
+        <location filename="erwin.cpp" line="454"/>
         <source>%1, no more recovery codes available!</source>
         <translation>%1, больше кодов восстановления не осталось!</translation>
     </message>
     <message>
-        <source>%1, stopping BOTHUNTER daemon. Good luck :3</source>
-        <translation type="vanished">%1, демон BOTHUNTER останавливается. Успехов! :3</translation>
+        <source>%1, stopping erwin daemon. Good luck :3</source>
+        <translation type="vanished">%1, демон erwin останавливается. Успехов! :3</translation>
     </message>
     <message>
-        <source>%1, BOTHUNTER will disconnect and restart after 10 seconds.</source>
+        <source>%1, erwin will disconnect and restart after 10 seconds.</source>
         <translation type="vanished">%1, выполняю перезапуск сервиса. Соединение будет восстановлено через 10 секунд.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="476"/>
+        <location filename="erwin.cpp" line="466"/>
         <source>%1, stopping the daemon. Good luck :3</source>
         <translation>%1, останавливаю сервис. До встречи! :3</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="485"/>
+        <location filename="erwin.cpp" line="475"/>
         <source>%1, daemon will disconnect and restart after 10 seconds.</source>
         <translation>%1, выполняю перезапуск сервиса. Соединение будет восстановлено через 10 секунд.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="495"/>
-        <source>%1, BOTHUNTER will disconnect and connect again, please wait...</source>
-        <translation>%1, BOTHUNTER выполняет переподключение, подождите...</translation>
+        <source>%1, erwin will disconnect and connect again, please wait...</source>
+        <translation type="vanished">%1, erwin выполняет переподключение, подождите...</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="500"/>
+        <location filename="erwin.cpp" line="490"/>
         <source>%1, usage: /daemon {restart|stop}</source>
         <translation>%1, использование: /daemon {restart|stop}</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="512"/>
+        <location filename="erwin.cpp" line="502"/>
         <source>%1, disable AUTOKICK first: /autokick off</source>
         <translation>%1, сначала отключи автоматическую выгонялку командой /autokick off</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="517"/>
+        <location filename="erwin.cpp" line="507"/>
         <source>%1, automatic BAN enabled. I will BAN everybody who comes in.</source>
         <translation>%1, включена автоматическая блокировка. Все прилетающие персонажи будут заблокированы в n/a.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="525"/>
+        <location filename="erwin.cpp" line="515"/>
         <source>%1, automatic BAN disabled.</source>
         <translation>%1, автоматическая блокировка отключена.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="529"/>
+        <location filename="erwin.cpp" line="519"/>
         <source>%1, usage: /autoban {on|off}</source>
         <translation>%1, использование: /autoban {on|off}</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="543"/>
+        <location filename="erwin.cpp" line="533"/>
         <source>%1, disable AUTOBAN first: /autoban off</source>
         <translation>%1, сначала отключи автоматическую блокировку командой /autoban off</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="548"/>
+        <location filename="erwin.cpp" line="538"/>
         <source>%1, automatic KICK enabled. I will KICK everybody who comes in.</source>
         <translation>%1, включена автоматическая выгонялка. Все прилетающие персонажи будут выгнаны без блокировки.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="556"/>
+        <location filename="erwin.cpp" line="546"/>
         <source>%1, automatic KICK disabled.</source>
         <translation>%1, автоматическая выгонялка отключена.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="560"/>
+        <location filename="erwin.cpp" line="550"/>
         <source>%1, usage: /autokick {on|off}</source>
         <translation>%1, использование: /autokick {on|off}</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="572"/>
+        <location filename="erwin.cpp" line="562"/>
         <source>%1, prison ban enabled. I will ban all the motherfuckers that prison others!</source>
         <translation>%1, включена блокировка за посадку. Я буду выгонять всех ублюдков-сажатиков!</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="579"/>
+        <location filename="erwin.cpp" line="569"/>
         <source>%1, prisonban disabled.</source>
         <translation>%1, блокировка за посадку отключена.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="583"/>
-        <location filename="bothunter.cpp" line="606"/>
+        <location filename="erwin.cpp" line="573"/>
+        <location filename="erwin.cpp" line="596"/>
         <source>%1, usage: /prisonban {on|off}</source>
         <translation>%1, использование: /prisonban {on|off}</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="594"/>
+        <location filename="erwin.cpp" line="584"/>
         <source>%1, silent mode enabled, you won&apos;t receive any messages from the bot anymore.</source>
         <translation>%1, включен тихий режим, сообщений от бота больше не будет.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="601"/>
+        <location filename="erwin.cpp" line="592"/>
         <source>%1, silent mode disabled.</source>
         <translation>%1, тихий режим отключен.</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="624"/>
+        <location filename="erwin.cpp" line="614"/>
         <source>%1, will ban all the nicks matching patterns: %2</source>
         <translation>%1, блокируем ники по маскам: %2</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="636"/>
+        <location filename="erwin.cpp" line="626"/>
         <source>%1, will ban all the clans matching patterns: %2</source>
         <translation>%1, блокируем кланы по маскам: %2</translation>
     </message>
     <message>
-        <location filename="bothunter.cpp" line="641"/>
+        <location filename="erwin.cpp" line="631"/>
         <source>%1, usage: /list {nicks|clans}</source>
         <translation>%1, использование: /list {nicks|clans}</translation>
     </message>
